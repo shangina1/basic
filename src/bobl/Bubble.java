cbf692910da100991f0c990916fc922c79bb93a6
pointmode <125463819+shangina1@users.noreply.github.com>
2024-11-19 11:19:18 +0300

    Update Bubble.java

--- src/bobl/Bubble.java
+++ src/bobl/Bubble.java
@@ -1,4 +1,19 @@
 package bobl;
 
-public class Bubble {
-}
+public class Bubble
+{
+        public static void main(String[] args)
+  {
+            Array one = new Array(4);
+            one.add(21);
+            one.add(3);
+            one.add(0);
+            one.add(134);
+            one.add(8);
+            one.add(99);
+            //    one.bubble();
+            one.display();
+            one.bubble();
+            one.display();
+        }
+    }
